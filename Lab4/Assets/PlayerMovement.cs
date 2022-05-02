a9c25191a9a2abddbf711fe290b0733941eff697
Simon Gelber <sgelber@calpoly.edu>
2022-05-02 11:02:28 -0700

    Added Sprite and Lighting Shader

---
+++ Lab4/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private float Move;
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed*Move,rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump")){
+            rb.AddForce(new Vector2(rb.velocity.x,jump));
+        }
+    }
+}
