eefd5167b2cedf542b6a6b40da914a48e2a1b77a
Simon Gelber <sgelber@calpoly.edu>
2022-05-04 10:55:08 -0700

    Added Animation

--- Lab4/Assets/PlayerMovement.cs
+++ Lab4/Assets/PlayerMovement.cs
@@ -8,12 +8,15 @@ public class PlayerMovement : MonoBehaviour
     public float jump;
 
     private float Move;
-
+    private Animator anim;
     public Rigidbody rb;
+
+    private CapsuleCollider cap;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        cap = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -24,7 +27,20 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(speed*Move,rb.velocity.y);
 
         if(Input.GetButtonDown("Jump")){
+            anim.SetTrigger("isJumping");
             rb.AddForce(new Vector2(rb.velocity.x,jump));
+            
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            anim.SetBool("isDucking", true);
+            //cap.height = 0.2f;
+            
+            
+        }else if(Input.GetKeyUp(KeyCode.DownArrow)){
+            anim.SetBool("isDucking", false);
+            
+            
         }
     }
 }
