282942b66fb6caf63e2424ff8226773e60eb461d
russellsiu <37996716+russellsiu@users.noreply.github.com>
2022-05-04 17:34:06 -0700

    Basic Start, Restart and Finish

--- Lab4/Assets/Scripts/PlayerMovement.cs
+++ Lab4/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -41,6 +42,11 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool("isDucking", false);
             
             
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
